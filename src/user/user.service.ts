10d4b598b0f64af9763c7005f2c2d63ae92cdd41
Israel David <israelcaetano21@hotmail.com>
2023-03-27 20:39:36 -0300

    feat(user): implement find-by-email method

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -26,7 +26,7 @@ export class UserService {
     return `This action returns all user`;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  findByEmail(email: string) {
+    return this.prisma.user.findUnique({ where: { email } });
   }
 }
