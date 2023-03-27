bc56e071fe38ae2e4bf27dc5a8958d89ea33e67b
Israel David <israelcaetano21@hotmail.com>
2023-03-27 19:22:28 -0300

    feat(prisma): create module and service

---
+++ prisma/migrations/20230327220542_init/migration.sql
@@ -0,0 +1,17 @@
+-- CreateTable
+CREATE TABLE `User` (
+    `id` VARCHAR(191) NOT NULL,
+    `email` VARCHAR(191) NOT NULL,
+    `name` VARCHAR(191) NOT NULL,
+    `role` VARCHAR(191) NULL,
+    `status` BOOLEAN NULL DEFAULT false,
+    `password` VARCHAR(191) NOT NULL,
+    `salt` VARCHAR(191) NOT NULL,
+    `confirmationToken` VARCHAR(191) NULL,
+    `recoverToken` VARCHAR(191) NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    UNIQUE INDEX `User_email_key`(`email`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
